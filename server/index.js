647c3e59be286fde21cd915d836735309bdbc5b2
Shreyansh <shreyansh.rajput2000@gmail.com>
2024-02-14 09:42:43 +0530

    Folder structure ceated

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
-const express = require('express')
-
+const express = require('express');
+// const { quizRouter, authRouter, adminRouter } = require('./routes');
+const cors = require('cors');
 const app = express();
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/',(req,res)=>{
-    console.log(typeof req)
-    res.send(JSON.stringify(req))
-})
+app.use(express.json());
+
+app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      optionsSuccessStatus: 204,
+    })
+  );
+  
+
+// app.use('/auth', authRouter);
+// app.use('/admin', adminRouter);
+// app.use('/quiz', quizRouter);
 
 app.listen(PORT,()=>{console.log(`Server started at ${PORT}`)})
---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+// const authRouter = require('./authRoutes');
+// const adminRouter = require('./adminRoutes');
+// const quizRouter = require('./quizRoutes');
+
+// module.exports = {
+//   authRouter,
+//   adminRouter,
+//   quizRouter,
+// };
