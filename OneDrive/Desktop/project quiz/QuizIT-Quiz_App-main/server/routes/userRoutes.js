b3216a58a731b181dc6353b32daaf94ec0a5b75e
Shreyansh Singh Bhadoriya <113119891+beingShreyansh@users.noreply.github.com>
2024-02-20 18:53:51 +0530

    Revert "auth added"

--- OneDrive/Desktop/project quiz/QuizIT-Quiz_App-main/server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-const { getUserQuizHistory} = require('../controllers/userControllers');
-
-router.post('/getHistory/:id', getUserQuizHistory);
-
-
-module.exports = router;
