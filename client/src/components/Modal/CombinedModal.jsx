a080ee5dbe90cbaf5cde7605bb349f594fe7c5e1
raksha <rakshapunasiya9@gmial>
2024-03-22 00:12:23 +0530

    updateQuestionAndOption

--- client/src/components/Modal/CombinedModal.jsx
+++ client/src/components/Modal/CombinedModal.jsx
@@ -5,11 +5,6 @@ import { faTimes, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 import "./modal.css"; // Import the CSS file
 import UpdateQuestionForm from "./UpdateQuetionForm"; // Import the UpdateQuestionForm component
 import axios from "axios";
-<<<<<<< HEAD
-//import NoData from "../../../assets/No-data.png";
-=======
-import NoData from "../../assets/No-data.png";
->>>>>>> d47e3a62a962ecf212a8c61eee91e9a9958962c6
 
 function CombinedModal({
   isOpen,
@@ -23,7 +18,7 @@ function CombinedModal({
   const [updateFormOpen, setUpdateFormOpen] = useState(false);
   const [selectedQuestion, setSelectedQuestion] = useState(null); // New state to store selected question data
   const modalContentRef = useRef(null);
- // const [isNoData, setIsNoData] = useState(false);
+
   const openUpdateForm = (questionId) => {
     setSelectedQuestionId(questionId);
     const question = selectedQuiz.find((q) => q.question_id === questionId); // Find the selected question data
@@ -81,57 +76,7 @@ function CombinedModal({
           </button>
         </div>
         <div className="modal-body">
-<<<<<<< HEAD
-        {/*{isNoData ? (
-                        <div style={{ display: 'flex' }}>
-                            <img src={NoData} alt="No data" style={{ margin: 'auto' }} />
-                        </div>
-                    ) : (*/}
-          <table className="modal-table">
-            <thead>
-              <tr>
-                <th>Question Content</th>
-                <th>Option A</th>
-                <th>Option B</th>
-                <th>Option C</th>
-                <th>Option D</th>
-                <th>Option E</th>
-                <th>Image URL</th> {/* Added Image URL column */}
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {selectedQuiz?.map((question, index) => (
-                <tr key={question.question_id}>
-                  <td>{question.question_content || "-"}</td>
-                  <td>{question.option_1 || "-"}</td>
-                  <td>{question.option_2 || "-"}</td>
-                  <td>{question.option_3 || "-"}</td>
-                  <td>{question.option_4 || "-"}</td>
-                  <td>{question.option_5 || "-"}</td>
-                  <td>{question.imageId || "-"}</td> {/* Display Image URL */}
-                  <td>
-                    <FontAwesomeIcon
-                      icon={faEdit}
-                      onClick={() => openUpdateForm(question.question_id)}
-                      style={{ marginRight: "10px" }}
-                    />
-                    <FontAwesomeIcon
-                      icon={faTrash}
-                      onClick={() => deleteQuestion(question.question_id)}
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-                    
-=======
-          {isNoData ? (
-            <div style={{ display: "flex" }}>
-              <img src={NoData} alt="No data" style={{ margin: "auto" }} />
-            </div>
-          ) : (
+         
             <table className="modal-table">
               <thead>
                 <tr>
@@ -141,7 +86,6 @@ function CombinedModal({
                   <th>Option C</th>
                   <th>Option D</th>
                   <th>Option E</th>
-                  <th>Image URL</th> {/* Added Image URL column */}
                   <th>Actions</th>
                 </tr>
               </thead>
@@ -154,7 +98,7 @@ function CombinedModal({
                     <td>{question.option_3 || "-"}</td>
                     <td>{question.option_4 || "-"}</td>
                     <td>{question.option_5 || "-"}</td>
-                    <td>{question.imageId || "-"}</td> {/* Display Image URL */}
+                  
                     <td>
                       <FontAwesomeIcon
                         icon={faEdit}
@@ -170,8 +114,7 @@ function CombinedModal({
                 ))}
               </tbody>
             </table>
-          )}
->>>>>>> d47e3a62a962ecf212a8c61eee91e9a9958962c6
+          
         </div>
       </div>
       {updateFormOpen &&
